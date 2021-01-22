1c4c1af3c2f3e204b93426dc42609e5444da8032
Jordan Ashbacher <jordan.ashbacher@gmail.com>
2021-01-22 15:44:12 -0600

    installs dependencies and sets up redux container on index

--- src/index.js
+++ src/index.js
@@ -2,7 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
-import registerServiceWorker from './registerServiceWorker';
+import {createStore, combineReducers, applyMiddleware} from 'redux'
+import {Provider} from 'react-redux'
+import logger from 'redux-logger'
+
+// redux container to collect form data
+const feedbackReducer = (state = {
+    feeling: '',
+    understanding: '',
+    support: '',
+    comments: ''
+}, action) => {
+    switch (action.type) {
+        case 'SET_FEELING':
+            return state.feeling = action.payload.feeling
+        case 'SET_UNDERSTANDING':
+            return action.payload;
+        case 'SET_SUPPORT':
+            return action.payload;
+        case 'SET_COMMENTS':
+            return action.payload;
+        case 'RESET':
+            return state = {feeling: '', understanding: '', support: '', comments: ''}
+        default:
+            return state;
+    }
+}
+
+const reduxStore = createStore(
+    combineReducers({
+        feedbackReducer
+    })
+)
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
