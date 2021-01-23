f5794a082a7912f9750e9f2d7fd4a3f5ddd4d1c9
Jordan Ashbacher <jordan.ashbacher@gmail.com>
2021-01-23 14:57:19 -0600

    changes number input to slider and adds styling to slider in form components

--- src/components/UnderstandingForm/UnderstandingForm.jsx
+++ src/components/UnderstandingForm/UnderstandingForm.jsx
@@ -1,16 +1,42 @@
-import {useState} from 'react'
-import {useDispatch} from 'react-redux'
-import {useHistory} from 'react-router-dom'
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+import { makeStyles } from '@material-ui/core/styles'
+import Slider from '@material-ui/core/Slider'
+import SentimentVeryDissatisfied from '@material-ui/icons/SentimentVeryDissatisfied'
+import SentimentVerySatisfied from '@material-ui/icons/SentimentVerySatisfied'
+import '../UnderstandingForm/UnderstandingForm.css'
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: 500,
+        margin: 30
+    },
+    margin: {
+        height: theme.spacing(3)
+    },
+}))
+
+const marks = [
+    {
+        value: 1,
+        label: <SentimentVeryDissatisfied />
+    },
+    {
+        value: 5,
+        label: <SentimentVerySatisfied />
+    }
+]
 
 const UnderstandingForm = () => {
 
     const dispatch = useDispatch()
     const history = useHistory()
+    const classes = useStyles()
 
-    const [understanding, setUnderstanding] = useState('')
+    const [understanding, setUnderstanding] = useState(1)
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
+    const handleNext = () => {
         console.log('in understanding handleSubmit')
         const action = {
             type: 'SET_UNDERSTANDING',
@@ -22,23 +48,28 @@ const UnderstandingForm = () => {
         history.push('/support')
     }
 
-    return(
-        <>
-        <h1>How well are you understanding the content?</h1>
-        <form onSubmit={handleSubmit}>
-            <input 
-                type="number" 
-                id="understandingIn" 
-                min="1" 
-                max="10" 
-                onChange={(e) => setUnderstanding(e.target.value)}
-                value={understanding}
-                required
-            />
-            <button type="submit">NEXT</button>
-        </form>
-            
-        </>
+    const handleChange = (event, newValue) => {
+        setUnderstanding(newValue)
+    }
+
+    return (
+        <div className="formContainer">
+            <h1>How well are you understanding the content?</h1>
+            <div className={classes.root}>
+                <Slider
+                    value={understanding}
+                    aria-labelledby="understandingSlider"
+                    step={1}
+                    min={1}
+                    max={5}
+                    marks={marks}
+                    valueLabelDisplay="on"
+                    onChange={handleChange}
+                />
+            </div>
+            <button onClick={handleNext}>NEXT</button>
+
+        </div>
     )
 }
 
