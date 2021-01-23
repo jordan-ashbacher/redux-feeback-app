d33767caacf037debe4e4e9615e724d8c8bb7e34
Jordan Ashbacher <jordan.ashbacher@gmail.com>
2021-01-22 18:02:31 -0600

    creates UnderstandingForm and SupportForm component framework

--- src/components/UnderstandingForm/UnderstandingForm.jsx
+++ src/components/UnderstandingForm/UnderstandingForm.jsx
@@ -2,37 +2,37 @@ import {useState} from 'react'
 import {useDispatch} from 'react-redux'
 import {useHistory} from 'react-router-dom'
 
-const FeelingForm = () => {
+const UnderstandingForm = () => {
 
     const dispatch = useDispatch()
     const history = useHistory()
 
-    const [feeling, setFeeling] = useState('')
+    const [understanding, setUnderstanding] = useState('')
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log('in feeling handle submit')
+        console.log('in understanding handleSubmit')
         const action = {
-            type: 'SET_FEELING',
-            payload: Number(feeling)
+            type: 'SET_UNDERSTANDING',
+            payload: Number(understanding)
         }
 
         dispatch(action)
-        setFeeling('')
-        history.push('/understanding')
+        setUnderstanding('')
+        history.push('/support')
     }
 
     return(
         <>
-        <h1>How are you feeling today?</h1>
+        <h1>How well are you understanding the content?</h1>
         <form onSubmit={handleSubmit}>
             <input 
                 type="number" 
-                id="feelingIn" 
+                id="understandingIn" 
                 min="1" 
                 max="10" 
-                onChange={(e) => setFeeling(e.target.value)}
-                value={feeling}
+                onChange={(e) => setUnderstanding(e.target.value)}
+                value={understanding}
             />
             <button type="submit">NEXT</button>
         </form>
@@ -41,4 +41,4 @@ const FeelingForm = () => {
     )
 }
 
-export default FeelingForm
+export default UnderstandingForm
